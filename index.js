5e02fc51ca45625992238f1b0ba9bbbd72d511cc
vnadao <vandao12031995@gmail.com>
2024-11-01 18:28:12 +0700

    feat: improve code

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-const puppeteer = require('puppeteer-extra');
-const stealthPlugin = require('puppeteer-extra-plugin-stealth')();
+const puppeteer = require("puppeteer-extra");
+const stealthPlugin = require("puppeteer-extra-plugin-stealth")();
 const { executablePath } = require('puppeteer');
 const chromePaths = require('chrome-paths');
-const fs = require('fs');
 const axios = require('axios');
+const fs = require('fs').promises;
+const path = require('path');
 
 puppeteer.use(stealthPlugin);
 
 const chromePath = chromePaths.chrome;
 
+
+async function loadCookiesFromFile() {
+  try {
+    // Read the cookie file
+    const filePath = path.join(__dirname, 'cookie.txt');
+    const cookieData = await fs.readFile(filePath, 'utf-8');
+    const cookies = cookieData.split('; ').map(cookie => {
+      const [name, ...valueParts] = cookie.split('=');
+      return {
+        name,
+        value: valueParts.join('='),
+        domain: 'www.nhatot.com',  // Specify the domain for the cookies
+        path: '/',
+        httpOnly: false,
+        secure: true,
+        sameSite: 'Lax',
+      };
+    });
+    return cookies;
+  } catch (error) {
+    console.error("Failed to load cookies from file:", error);
+    return [];
+  }
+}
+
 async function getPhoneNumber(postId) {
   const postUrl = `https://www.nhatot.com/${postId}.htm`;
 
   const browser = await puppeteer.launch({
-    executablePath: chromePath,
-    headless: true,
-    args: ['--window-size=400,1000'],
-  });
-  try {
+            executablePath: chromePath,
+            headless: true,
+            args: [
+              '--window-size=400,1000'
+            ],
+          });
+  try{
     const page = (await browser.pages())[0];
+    
     await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
     );
-    await page.goto(postUrl);
+
+    // Split and map each cookie string to the format required by Puppeteer
+    const cookies = await loadCookiesFromFile()
+    
+    // Set all cookies on the page
+    await page.setCookie(...cookies);
+
+    await page.goto(postUrl, { waitUntil: 'networkidle2' });
+
+    // Add a general wait for a known element on the page to ensure it's fully loaded
+    await page.waitForSelector('[class^="InlineShowPhoneButton"]', { timeout: 10000 });
+
+
     var phoneNumber = '';
     page.on('response', async (response) => {
       // Log the response data
@@ -39,9 +80,11 @@ async function getPhoneNumber(postId) {
     });
 
     var btnGetPhone = await page.waitForSelector(
-      '[class^="InlineShowPhoneButton_linkContact"] button',
+      '[class^="InlineShowPhoneButton"] button',
       { timeout: 5000 }
     );
+
+    console.log(btnGetPhone)
     await btnGetPhone.click();
     await page.waitForTimeout(700);
     await browser.close();
@@ -128,6 +171,11 @@ async function deletePhoneNumber(postId) {
       console.log('deletePhoneNumber error');
       console.log(error);
     });
+}
+
+async function test_run() {
+  postId = '120626246'
+  await updatePhoneNumber(postId);
 }
 
 async function run() {
